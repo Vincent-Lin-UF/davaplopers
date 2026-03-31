b2da65546796c90671ee572a62f1515c27ce793e
destingollamudi <destingollamudi@gmail.com>
2026-03-31 16:22:44 -0400

    add login component and route; initialize Google OAuth

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup';
 import { ChatComponent } from './chat/chat';
+import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: SignupComponent },
   { path: 'chat', component: ChatComponent },
+  { path: 'login', component: LoginComponent }
 ];
